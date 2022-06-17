3e14f9d01dfc69d3af60d95d53d313dafd0cc118
yusukedayo <tsunemi0217@icloud.com>
2022-06-17 17:43:30 +0900

    Change: 出席に失敗した場合はそのままredirectされるように変更

--- app/controllers/events/attendances_controller.rb
+++ app/controllers/events/attendances_controller.rb
@@ -3,15 +3,15 @@
 class Events::AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    if @event.check(current_user)
-      redirect_back(fallback_location: root_path, success: '参加に失敗しました 性別欄を確認してください')
-    else
       event_attendance = current_user.attend(@event)
-      (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
-        NotificationFacade.attended_to_event(event_attendance, user)
+      unless event_attendance.id == nil
+        (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
+          NotificationFacade.attended_to_event(event_attendance, user)
+        end
+        redirect_back(fallback_location: root_path, success: '参加の申込をしました')
+      else
+        redirect_back(fallback_location: root_path, danger: "#{event_attendance.adjust_error_message}")
       end
-      redirect_back(fallback_location: root_path, success: '参加の申込をしました')
-    end
   end
 
   def destroy
