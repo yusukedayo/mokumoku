ac90fe9ec17da7162b002a17b23b58ab41595a91
yusukedayo <tsunemi0217@icloud.com>
2022-06-18 17:20:10 +0900

    Change: メソッドを統一

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -7,8 +7,7 @@ class EventAttendance < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :event_id }
   validate :check_set_gender
-  validate :check_set_woman
-  validate :check_set_man
+  validate :check_gender
 
   def check_set_gender
     return unless Event.find(event_id).gender_ratio != 'not_set' && User.find(user_id).gender == 'not_set'
@@ -16,16 +15,12 @@ class EventAttendance < ApplicationRecord
     errors.add(:user_id, 'このもくもく会には性別を設定してから申し込んでください')
   end
 
-  def check_set_woman
-    return unless Event.find(event_id).gender_ratio == 'only_woman' && User.find(user_id).gender == 'man'
-
-    errors.add(:user_id, 'このもくもく会は女性限定です')
-  end
-
-  def check_set_man
-    return unless Event.find(event_id).gender_ratio == 'only_man' && User.find(user_id).gender == 'woman'
-
-    errors.add(:user_id, 'このもくもく会は男性限定です')
+  def check_gender
+    if Event.find(event_id).gender_ratio == 'only_woman' && User.find(user_id).gender == 'man'
+      errors.add(:user_id, 'このもくもく会は女性限定です')
+    elsif Event.find(event_id).gender_ratio == 'only_man' && User.find(user_id).gender == 'woman'
+      errors.add(:user_id, 'このもくもく会は男性限定です')
+    end
   end
 
   def adjust_error_message
