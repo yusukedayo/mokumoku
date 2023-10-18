1e035aae290be9fe0cfe1e16d609826827d4ac7b
Mikiko9882 <teddy.0802.t@gmail.com>
2023-10-18 20:48:54 +0900

    タグを登録するためのコントローラを修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,7 @@ class EventsController < ApplicationController
   def create
     @event = current_user.events.build(event_params)
     if @event.save
+      @event.tag_ids = params[:event][:tag_ids]
       User.all.find_each do |user|
         NotificationFacade.created_event(@event, user)
       end
@@ -60,6 +61,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :content, :held_at, :prefecture_id, :thumbnail)
+    params.require(:event).permit(:title, :content, :held_at, :prefecture_id, :thumbnail, tag_ids: [])
   end
 end
